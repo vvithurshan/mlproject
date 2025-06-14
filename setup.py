30536cb6224c7c3d51ee0f57d3c43a561a9c8ecd
vvithurshan <vvithurshan@gmail.com>
2025-06-13 19:32:37 -0600

    Setup Updated

--- setup.py
+++ setup.py
@@ -1,10 +1,24 @@
 from setuptools import find_packages, setup
+from typing import List
+
+def get_requirments(file_path:str)->List[str]:
+    '''
+    this function will return the list of requirments
+    '''
+    requirments = []
+    with open(file_path) as file_obj:
+        requirments = file_obj.readlines()
+        requirments = [req.replace("\n", "") for req in requirments ]
+        if '-e .' in requirments:
+            requirments.remove('-e .')
+    return requirments
 
 setup(
     name='mlprojct',
     versions='0.0.1',
     author='Vithurshan',
     author_email='vvithurshan@gmail.com',
-    packages=find_packages()
-    install_requires=['pandas', 'numpy', 'seaborn', 'matplot']
+    packages=find_packages(),
+    #install_requires=['pandas', 'numpy', 'seaborn', 'matplot']
+    install_requires=get_requirments('requirments.txt')
 )
