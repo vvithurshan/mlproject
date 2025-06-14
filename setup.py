bc18d6d093c1bb43531a537c4a8773f847244560
vvithurshan <vvithurshan@gmail.com>
2025-06-13 19:14:32 -0600

    Setup Added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='mlprojct',
+    versions='0.0.1',
+    author='Vithurshan',
+    author_email='vvithurshan@gmail.com',
+    packages=find_packages()
+    install_requires=['pandas', 'numpy', 'seaborn', 'matplot']
+)
