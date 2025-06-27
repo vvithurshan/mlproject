033a21de6db0aaf84815e67d3893bc5b0d864321
vvithurshan <vvithurshan@gmail.com>
2025-06-27 06:16:54 -0600

    Hyperparameter Tuining Done

--- src/utils.py
+++ src/utils.py
@@ -7,7 +7,7 @@ import dill
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.metrics import mean_squared_error, r2_score
-
+from sklearn.model_selection import GridSearchCV
 
 def save_object(file_path, obj):
     try:
@@ -20,21 +20,27 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_model(x_train, y_train, x_test, y_test, models):
+def evaluate_model(x_train, y_train, x_test, y_test, models, params):
     try:
         report = {}
 
-        for i in range(len(models)):
-            model = list(models.values())[i]
-
+        for model_name, model_value in models.items():
+        
+            model = model_value
+            param = params.get(model_name, {})
+            logging.info(f'{model} Training Started with {param}')
+            grid = GridSearchCV(model, param, cv = 3)
+            grid.fit(x_train, y_train)
+            model.set_params(**grid.best_params_)
             model.fit(x_train, y_train)
+            logging.info(f'{model} Training Over')
             y_train_pred = model.predict(x_train)
             y_test_pred = model.predict(x_test)
 
             train_model_score = r2_score(y_train, y_train_pred)
             test_model_score = r2_score(y_test, y_test_pred)
 
-            report[list(models.keys())[i]] = test_model_score
+            report[model_name] = test_model_score
         return report 
     
     except Exception as e:
